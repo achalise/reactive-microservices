c7cc9db60f7792aa447f6c88e25c438def554b52
Arun Chalise <Arun.Chalise@macquarie.com>
2017-12-05 10:54:58 +1100

    Added scaffolding for front end using Angular 5 and ng-bootstrap

---
+++ front-end/ebanking-app/src/app/app-navbar/app-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-navbar',
+  templateUrl: './app-navbar.component.html',
+  styleUrls: ['./app-navbar.component.scss']
+})
+export class AppNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
